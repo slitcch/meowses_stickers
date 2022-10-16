6dd5e39be9a359c8ab308e747ef8470d898b6f43
Moses Turner <moses@collabora.com>
2022-10-15 20:57:27 -0500

    im scared

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '995ad5a9'
+version = '0.1.0+dev.995ad5a9'
+linkified_version = '0.1.0+dev.[995ad5a9](https://github.com/maunium/stickerpicker/commit/995ad5a9d19d644eaff5c3ab14a5a440f74ccc32)'
